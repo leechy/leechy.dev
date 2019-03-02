3975cc6bca95733ebdbb6b5871453c7cd639bd39
Andrey Lechev <leechy@leechy.ru>
2019-03-02 16:01:11 +0100

    Add Firebase keys to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCu6Tl1HnW7H-4qDOM0jTsupbu4jL9cUX4',
+    authDomain: 'leechy-dev.firebaseapp.com',
+    databaseURL: 'https://leechy-dev.firebaseio.com',
+    projectId: 'leechy-dev',
+    storageBucket: 'leechy-dev.appspot.com',
+    messagingSenderId: '535273195021'
+  }
 };
 
 /*
