c96f5bd2f50ac56f4786d796c25a53a89bfd36fb
Andrey Lechev <leechy@leechy.ru>
2019-03-02 17:33:29 +0100

    Add logic to Articles List Component

--- src/app/components/articles-list/articles-list.component.ts
+++ src/app/components/articles-list/articles-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from 'src/app/services/articles.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles-list.component.scss']
 })
 export class ArticlesListComponent implements OnInit {
+  articles = [];
+  articlesSubscription: Subscription;
 
-  constructor() { }
+  constructor(private articlesService: ArticlesService) {}
 
   ngOnInit() {
+    // load index article (there's articles list)
+    this.articlesService.get('index');
+    this.articlesSubscription = this.articlesService.data.subscribe(data => {
+      if (data && data['index']) {
+        this.articles = data['index'].articles;
+        console.log('articles', this.articles);
+      }
+    });
   }
-
 }
